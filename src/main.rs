f6d9cd1c25253596afe77ddf44e8819b85fd8229
Roger Andersen <hexjelly@hexjelly.com>
2017-03-16 22:50:08 +0100

    fix: separate out circle creation into own function

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ struct Vertex {
     position: [f64; 2],
 }
 
+const PI2: f64 = std::f64::consts::PI * 2.;
+
+fn make_circle(center: [f64; 2], radius: f64, triangle_count: u8) -> Vec<Vertex> {
+    let mut circle_vertices = vec![];
+
+    circle_vertices.push(Vertex { position: [center[0], center[1]] });
+    for n in 0..triangle_count + 1 {
+        circle_vertices.push(Vertex { position: [center[0] + (radius * (n as f64 * PI2 / triangle_count as f64).cos()),
+                                                 center[1] + (radius * (n as f64 * PI2 / triangle_count as f64).sin())] });
+        }
+
+    circle_vertices
+}
+
 fn main() {
     let display = glium::glutin::WindowBuilder::new()
         .with_dimensions(500, 500)
@@ -16,17 +30,7 @@ fn main() {
 
     implement_vertex!(Vertex, position);
 
-    let circle_detail = 30_u8;
-    let circle_radius = 0.2_f64;
-    let circle_center = [0_f64, 0_f64];
-    let mut circle_vertices = vec![];
-    let twice_pi = std::f64::consts::PI * 2.;
-
-    circle_vertices.push(Vertex { position: [circle_center[0], circle_center[1]] });
-    for n in 0..circle_detail + 1 {
-        circle_vertices.push(Vertex { position: [circle_center[0] + (circle_radius * (n as f64 * twice_pi / circle_detail as f64).cos()),
-                             circle_center[1] + (circle_radius * (n as f64 * twice_pi / circle_detail as f64).sin())] });
-	}
+    let circle_one = make_circle([0.0, 0.0], 0.5, 50);
 
     let vertex_shader_src = r#"
         #version 140
@@ -47,7 +51,7 @@ fn main() {
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-    let vertex_buffer = glium::VertexBuffer::new(&display, &circle_vertices).unwrap();
+    let vertex_buffer = glium::VertexBuffer::new(&display, &circle_one).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleFan);
 
     loop {
