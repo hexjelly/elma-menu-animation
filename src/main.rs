a7e1988b9b05c2f1449cb3612abdb4650a7b3339
Roger Andersen <hexjelly@hexjelly.com>
2017-03-17 08:57:02 +0100

    feat: add Ball struct and some velocity testing

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,52 @@ extern crate glium;
 use glium::DisplayBuild;
 use glium::Surface;
 
-#[derive(Copy, Clone)]
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
+#[derive(Default, Copy, Clone)]
 struct Vertex {
     position: [f64; 2],
 }
 
 struct Ball {
-    position: Vertex,
-    velocity: [f64; 2],
+    vertex: Vertex,
+    radius: f64,
+    velocity: [f64; 3],
+    tris: u8,
+}
+
+impl Ball {
+    fn new(position: [f64; 2], radius: f64, velocity: [f64; 3]) -> Ball {
+        Ball {
+            vertex: Vertex { position: position },
+            radius: radius,
+            velocity: velocity,
+            tris: 50,
+        }
+    }
+
+    fn update(&mut self) {
+        self.vertex.position[0] += self.velocity[0];
+        self.vertex.position[1] += self.velocity[1];
+    }
+
+    fn render(&self) -> Vec<Vertex> {
+        let mut vertices = vec![];
+
+        vertices.push(self.vertex.clone());
+        for n in 0..self.tris + 1 {
+            vertices.push(Vertex {
+                position: [&self.vertex.position[0] + (&self.radius * (n as f64 * PI2 / self.tris as f64).cos()),
+                           &self.vertex.position[1] + (&self.radius * (n as f64 * PI2 / self.tris as f64).sin())],
+            });
+        }
+
+        vertices
+    }
 }
 
 const PI2: f64 = std::f64::consts::PI * 2.;
-
-fn make_circle(center: [f64; 2], radius: f64, triangle_count: u8) -> Vec<Vertex> {
-    let mut circle_vertices = vec![];
-
-    circle_vertices.push(Vertex { position: [center[0], center[1]] });
-    for n in 0..triangle_count + 1 {
-        circle_vertices.push(Vertex {
-            position: [center[0] + (radius * (n as f64 * PI2 / triangle_count as f64).cos()),
-                       center[1] + (radius * (n as f64 * PI2 / triangle_count as f64).sin())],
-        });
-    }
-
-    circle_vertices
-}
 
 fn main() {
     let display = glium::glutin::WindowBuilder::new()
@@ -39,9 +60,15 @@ fn main() {
 
     implement_vertex!(Vertex, position);
 
-    let circles = vec![make_circle([-0.375, 0.5], 0.075, 50), make_circle([0.0, 0.5], 0.09375, 50), make_circle([0.375, 0.5], 0.15625, 50),
-                       make_circle([-0.375, 0.0], 0.075, 50), make_circle([0.0, 0.0], 0.09375, 50), make_circle([0.375, 0.0], 0.15625, 50),
-                       make_circle([-0.375, -0.5], 0.075, 50), make_circle([0.0, -0.5], 0.09375, 50), make_circle([0.375, -0.5], 0.15625, 50)];
+    let rand_range = Range::new(0.028648_f64, 89.848719_f64);
+    let mut rng_gen = rand::thread_rng();
+    let rand_degree = rand_range.ind_sample(&mut rng_gen) * std::f64::consts::PI / 180.0; // deg to rad
+    let mut circles = vec![Ball::new([-0.375, 0.5], 0.075, [rand_degree.cos() / 5000.0, rand_degree.sin() / 5000.0, 0.0]),
+                        Ball::new([0.0, 0.5], 0.09375, [0.0, 0.0, 0.0]), Ball::new([0.375, 0.5], 0.15625, [0.0, 0.0, 0.0]),
+                       Ball::new([-0.375, 0.0], 0.075, [0.0, 0.0, 0.0]), Ball::new([0.0, 0.0], 0.09375, [0.0, 0.0, 0.0]),
+                       Ball::new([0.375, 0.0], 0.15625, [0.0, 0.0, 0.0]),
+                       Ball::new([-0.375, -0.5], 0.075, [0.0, 0.0, 0.0]), Ball::new([0.0, -0.5], 0.09375, [0.0, 0.0, 0.0]),
+                       Ball::new([0.375, -0.5], 0.15625, [0.0, 0.0, 0.0])];
 
     let vertex_shader_src = include_str!("shaders/ball.vert");
     let fragment_shader_src = include_str!("shaders/ball.frag");
@@ -50,15 +77,17 @@ fn main() {
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
-    let mut vbuffers = vec![];
-    for circle in circles {
-        vbuffers.push(glium::VertexBuffer::new(&display, &circle).unwrap());
-    }
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleFan);
 
     loop {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
+
+        let mut vbuffers = vec![];
+        for circle in &mut circles {
+            circle.update();
+            vbuffers.push(glium::VertexBuffer::new(&display, &circle.render()).unwrap());
+        }
 
         for buffer in &vbuffers {
             target.draw(buffer,
